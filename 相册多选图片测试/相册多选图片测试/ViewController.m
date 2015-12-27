8652c713f089f48be6e93a88627398b552d8ab63
章芝源 <610925874@qq.com>
2015-12-27 14:59:10 +0800

    多张图片,  相册选择

--- 相册多选图片测试/相册多选图片测试/ViewController.m
+++ 相册多选图片测试/相册多选图片测试/ViewController.m
@@ -16,35 +16,36 @@
 
 @implementation ViewController
 
-- (void)viewDidLoad {
-    [super viewDidLoad];
-//    NSLog(@"%@", [[UIDevice currentDevice] systemVersion]);
+//单张图片点击
+- (IBAction)onePicture:(id)sender {
     
-    
+    NSLog(@"为实现");
 }
 
-- (IBAction)onePictureClick:(id)sender {
-    
-    
-    
+- (void)viewDidLoad {
+    [super viewDidLoad];
 }
+
+//多张图片
 - (IBAction)MultiplisePictureClick:(id)sender {
     
     ELCImagePickerController *elcPicker = [[ELCImagePickerController alloc] initImagePicker];
     
-    elcPicker.maximumImagesCount = 100; //Set the maximum number of images to select to 100
-    elcPicker.returnsOriginalImage = YES; //Only return the fullScreenImage, not the fullResolutionImage
-    elcPicker.returnsImage = YES; //Return UIimage if YES. If NO, only return asset location information
-    elcPicker.onOrder = YES; //For multiple image selection, display and return order of selected images
-//    elcPicker.mediaTypes = @[(NSString *)kUTTypeImage, (NSString *)kUTTypeMovie]; //Supports image and movie types
+    elcPicker.maximumImagesCount = 100;  //最大选择数
+    elcPicker.returnsOriginalImage = YES;  //返回未编辑的图片
+    elcPicker.returnsImage = YES;  //返回的是图片
+    elcPicker.onOrder = YES;  //是否多选
+
     
     elcPicker.imagePickerDelegate = self;
     
+    //modal出相册控制器
     [self presentViewController:elcPicker animated:YES completion:nil];
 
 }
 
 #pragma mark -ELCImagePickerController代理方法
+//demo代理方法,  当选择完图片点击 Done的时候会被调用
 - (void)elcImagePickerController:(ELCImagePickerController *)picker didFinishPickingMediaWithInfo:(NSArray *)info
 {
     [self dismissViewControllerAnimated:YES completion:nil];
@@ -60,7 +61,7 @@
                 UIImage *image = dict[UIImagePickerControllerOriginalImage];
                 UIImageView *imageview = [[UIImageView alloc]initWithImage:image];
                 imageview.frame = imageFrame;
-                //添加到scrollView上
+                //将选择的图片添加到, scrollView上展示
                 [self.scrollView addSubview:imageview];
                 imageFrame.origin.x += self.scrollView.bounds.size.width;
             } else {
@@ -81,11 +82,13 @@
             NSLog(@"unknown asset type");
         }
     }
-    
+    //分页
     self.scrollView.pagingEnabled = YES;
+    //scrollView范围
     self.scrollView.contentSize = CGSizeMake(info.count * self.scrollView.bounds.size.width, self.scrollView.bounds.size.width);
 }
 
+//点击取消, 退出整个控制器
 - (void)elcImagePickerControllerDidCancel:(ELCImagePickerController *)picker
 {
     NSLog(@"取消");
